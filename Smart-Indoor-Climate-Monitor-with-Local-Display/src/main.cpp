ac7a0299bfabbe32593affb124f4c6a1df7d3e49
Kaeokk <kaeo.kloss@dawsoncollege.qc.ca>
2025-11-19 16:30:18 -0500

    Update main.cpp

--- Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
+++ Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
@@ -3,7 +3,16 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_SSD1306.h>
 
+#define OLED_MOSI 18
+#define OLED_CLK 5
+#define OLED_DC 2
+#define OLED_CS 17
+#define OLED_RES 4
 
+
+#define RedPin 14
+#define BluePin 26
+#define GreenPin 27
 
 
 void setup() {
