604b680e2b653887ef69e7cbc802af34f287978f
Anto345-pixel <antonio.lastoria@dawsoncollege.qc.ca>
2025-11-19 16:30:08 -0500

    Add
    Add

--- Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
+++ Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
@@ -3,7 +3,11 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_SSD1306.h>
 
+#define Screen_Width 128
+#define Screen_Height 64
 
+_Adafruit_SSD1306_H_ display(Screen_Width, Screen_Height, &Wire, -1);
+Adafruit_BME280 bme;
 
 
 void setup() {
