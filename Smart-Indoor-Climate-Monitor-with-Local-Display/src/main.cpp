404fe3b3105ea9abcbc75d939d2e327105bd4e36
Anto345-pixel <antonio.lastoria@dawsoncollege.qc.ca>
2025-11-19 16:39:20 -0500

    Update main.cpp

--- Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
+++ Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
@@ -6,8 +6,19 @@
 #define Screen_Width 128
 #define Screen_Height 64
 
-_Adafruit_SSD1306_H_ display(Screen_Width, Screen_Height, &Wire, -1);
+
 Adafruit_BME280 bme;
+
+int DIN = 18;
+int DC = 2;
+int CS = 17;
+int CLK = 5;
+int RES = 4;
+int RedPin = 14;
+int GreenPin = 27;
+int BluePin = 26;
+int BMESCL = 33;
+int BMESDA = 32;
 
 
 void setup() {
