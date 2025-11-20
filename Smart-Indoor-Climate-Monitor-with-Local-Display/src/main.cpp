09b5785f31ebaef9837a6d967eb4529720c127e9
Anto345-pixel <antonio.lastoria@dawsoncollege.qc.ca>
2025-11-19 20:16:10 -0500

    Added more
    Added more

--- Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
+++ Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
@@ -1,56 +1,73 @@
 #include <Arduino.h>
-#include <wire.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
 
 
-
-Adafruit_BME280 bme;
-
+// Defining OLED pinout
 int DIN = 18;
 int DC = 2;
 int CS = 17;
 int CLK = 5;
-int RES = 4;
+int RS = 4;
+
+// Defining RGB pinout
 int RedPin = 14;
 int GreenPin = 27;
 int BluePin = 26;
+
+// Defining BME sensor pinout
 int BMESCL = 33;
 int BMESDA = 32;
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS, DC, RS);
+
+#define BME280_ADDRESS 0x77
+Adafruit_BME280 bme;
 
 void setup() {
 
-Serial.println(F("SSD1306 allocation failed"));
-
-display.clearDisplay();
-display.setTextSize(1);
-display.setTextColor(WHITE);
-display.setCursor(0, 10);
-display.println("Hello, SSD1306!");
-display.display();
+Serial.begin(9600);
+Wire.begin(BMESDA, BMESCL);
+if (!bme.begin(BME280_ADDRESS)) {
+    Serial.println("The address cannot be found for the BME sensor");
+    while(1);
 }
 
 
+if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+}
+Serial.println("Starting BME280...");
 
-
-
-
-
-
-
-
+}
 void loop() {
 
 
 
 
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(SSD1306_WHITE);
+display.setCursor(0,0);
+display.print("T:");
+display.print(bme.readTemperature());
+display.println("C");
 
 
+display.print("H:");
+display.print(bme.readHumidity());
+display.println("%");
 
+display.print("P:");
+display.print(bme.readPressure());
+display.println("Pa");
+display.display();
 
 
 
