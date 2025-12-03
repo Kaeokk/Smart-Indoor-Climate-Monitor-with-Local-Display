8569c21e75532d648f4ad2ebc26985f3ead0bf63
Kaeokk <kaeo.kloss@dawsoncollege.qc.ca>
2025-12-03 13:19:27 -0500

    Update main.cpp

--- Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
+++ Smart-Indoor-Climate-Monitor-with-Local-Display/src/main.cpp
@@ -8,17 +8,13 @@
 #include <SPI.h>
 
 // Libraries for the web server
-
 #include <WiFi.h>
 #include <WebServer.h>
 
-//Wifi credentials
-const char* ssid = "Greg";
-const char* password = "password1234";
-
-// graph and numbers switch
-unsigned long lastSwitchTime = 0;
+// graph and numbers switch, start with show numbers
 bool showNumbers = true;
+//oled switching pin
+int buttonPin = 13;
 
 // Defining OLED pinout
 int DIN = 23;
@@ -28,7 +24,7 @@ int CLK = 18;
 int RS = 4;
 
 
-//OLED
+//OLED setup
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, DC, RS, CS);
@@ -37,24 +33,19 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, DC, RS, CS);
 // Defining RGB pinout
 int RedPin = 14;
 int GreenPin = 27;
-int BluePin = 26;
 
 // Defining BME sensor pinout
 int BMESCL = 33;
 int BMESDA = 32;
-
-//oled switching pin
-int buttonPin = 13;
-
-
-
-// BME280
-// #define BME280_ADDRESS 0x76
 Adafruit_BME280 bme;
 
 // Thresholds  for alerts (starting)
 float tempHigh = 28.0;
 float humHigh = 60.0;
+
+//Wifi credentials
+const char* ssid = "Greg";
+const char* password = "password1234";
 
 // WEB SERVER
 WebServer server(80);
@@ -64,18 +55,18 @@ float tempHistory[HISTORY_SIZE];
 float humHistory[HISTORY_SIZE];
 int historyIndex = 0;
 
+//set sensor read time to zero
 unsigned long lastSample = 0;
 
+//set up history
 void addToHistory(float temp, float hum) {
   tempHistory[historyIndex] = temp;
   humHistory[historyIndex] = hum;
-
   historyIndex++;
   if (historyIndex >= HISTORY_SIZE) historyIndex = 0;
 }
 
 // Draw one history graph curve
-
 void drawGraph(float *dataArray, float minVal, float maxVal) {
   for (int i = 1; i < HISTORY_SIZE; i++) {
 
@@ -83,7 +74,6 @@ void drawGraph(float *dataArray, float minVal, float maxVal) {
     int x2 = i;
     float v1 = dataArray[(historyIndex + i - 1) % HISTORY_SIZE];
     float v2 = dataArray[(historyIndex + i) % HISTORY_SIZE];
-
     int y1 = map(v1, minVal, maxVal, SCREEN_HEIGHT - 1, 10);
     int y2 = map(v2, minVal, maxVal, SCREEN_HEIGHT - 1, 10);
 
@@ -95,13 +85,14 @@ void drawGraph(float *dataArray, float minVal, float maxVal) {
 
 //function to switch oled
 void updateOLED() {
-  display.clearDisplay();
+display.clearDisplay();
 
-  if (showNumbers) {
-    //numbers only
-    display.setTextSize(2);
+ //numbers only
+if (showNumbers) {
+  // Display sensor readings in large numbers.
+    display.setTextSize(2); // Set font size.
     display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
+    display.setCursor(0, 0); // Set cursor for Temperature (T).
     display.print("T:");
     display.print(bme.readTemperature(), 1);
     display.println("C");
@@ -113,18 +104,17 @@ void updateOLED() {
 
     display.setCursor(0, 50);
     display.print("P:");
-    display.print((int)(bme.readPressure()/100));
+    display.print((int)(bme.readPressure()/100)); // Read and print Pressure in hPa.
     display.println("hPa");
   } else {
     //graph
     display.setTextSize(1);
     display.setCursor(0, 0);
     display.println("Temp & Hum Trend");
-
-    drawGraph(tempHistory, 15, 35);
-    drawGraph(humHistory,  30, 90);
+    drawGraph(tempHistory, 10, 40); //call on drawGraph, using history array.
+    drawGraph(humHistory,  20, 90); //call on drawGraph
   }
-
+  //display either
   display.display();
 }
 
@@ -134,35 +124,21 @@ Serial.begin(9600);
 //Leds
 pinMode(RedPin, OUTPUT);
 pinMode(GreenPin, OUTPUT);
-pinMode(BluePin, OUTPUT);
 
-  // OLED 
+  // OLED troubleshooting
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("OLED init failed");
     while (1);
   }
-
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.println("OLED OK");
-  display.display();
-  delay(1000);
+  // Then BME280 troubleshooting
+  Wire.begin(BMESDA, BMESCL);
+  if (!bme.begin(0x76) && !bme.begin(0x77)) {
+    Serial.println("BME280 not found");
+  }
 
   //oled switcing button
   pinMode(buttonPin, INPUT_PULLUP);
 
-  // Then BME280
-  Wire.begin(BMESDA, BMESCL);
-  if (!bme.begin(0x76) && !bme.begin(0x77)) {
-    Serial.println("BME280 not found");
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.println("Sensor offline");
-    display.display();
-    return; // Don't halt the program
-  }
 //initialize wifi
 WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
@@ -175,8 +151,9 @@ WiFi.begin(ssid, password);
   Serial.println(WiFi.localIP());
   Serial.println("All devices working");
 
-  // WEB SERVER
+// WEB SERVER...Define the handler for the root path ("/") of the web server. This is the main page users see.
 server.on("/", []() {
+// Send an HTTP response with status 200 (OK) and content type "text/html".
   server.send(200, "text/html",
 R"=====(<body>
 
@@ -198,49 +175,49 @@ setInterval(()=>{
 
 </body>)=====");
 });
-
-
-
+// This path only sends sensor data
   server.on("/data", [](){
+    // Send a simple, plain text response back to the browser
     server.send(200, "text/plain", 
+      // Reads the current Temperature and Humidity from the BME280 sensor as plain text
       String(bme.readTemperature(),1) + "," + String(bme.readHumidity(),1));
   });
-
+//This link updates the alarm limits on the board
   server.on("/set", [](){
+    // If the URL has a t value, update the codes tempHigh limit
     if(server.hasArg("t")) tempHigh = server.arg("t").toFloat();
+    // If the URL has an h value, update the codes humHigh limit
     if(server.hasArg("h")) humHigh  = server.arg("h").toFloat();
+    // Send a confirmation back to the browser
     server.send(200, "text:)", "OK");
   });
-
+// Start the web server now, so it can listen for users
   server.begin();                     
   Serial.println("Web server running");  
-
   // show first screen 
   updateOLED();   
 }
 
 void loop() {
-
-
 //call server
 server.handleClient();
 
 //READ VALUES ONCE
 float temp = bme.readTemperature(); 
 float hum  = bme.readHumidity();
-
-//basic led alerts
-  if (temp > tempHigh || hum > humHigh) {
-    digitalWrite(RedPin, HIGH);
-    digitalWrite(GreenPin, LOW);
-  } else {
-    digitalWrite(RedPin, LOW);
-    digitalWrite(GreenPin, HIGH);
-  }
-  // Record sensor every .2 seconds
+// Record sensor every .2 seconds
  if (millis() - lastSample > 200) {
     lastSample = millis();
     addToHistory(temp, hum);
+  }
+
+// led alerts
+  if (temp > tempHigh || hum > humHigh) {
+  digitalWrite(RedPin, HIGH);
+  digitalWrite(GreenPin, LOW);
+  } else {
+  digitalWrite(RedPin, LOW);
+  digitalWrite(GreenPin, HIGH); 
   }
 
 // BUTTON switching
@@ -249,10 +226,11 @@ float hum  = bme.readHumidity();
 
   if (lastButtonState == HIGH && buttonState == LOW) {  // falling edge
     showNumbers = !showNumbers;
+    updateOLED(); 
+    delay(50); 
   }
 
   lastButtonState = buttonState;
-
-  // Update OLED (every render call)
+    // Update OLED
   updateOLED();
 }
